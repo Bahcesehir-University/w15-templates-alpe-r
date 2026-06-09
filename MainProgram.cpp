2c1a96d20e4207904552ee30f09fb3426bcc9f78
alpe-r <alper.aktas@bahcesehir.edu.tr>
2026-06-09 14:43:14 +0300

    Update MainProgram.cpp

--- MainProgram.cpp
+++ MainProgram.cpp
@@ -136,7 +136,7 @@ public:
     // TODO 6c: set data_[index]; throw std::out_of_range if invalid
     void set(int index, const T& value) {
         if (index < 0 || index >= N)
-        throw std::out_of_range("FixedArray index out of range");
+         throw std::out_of_range("FixedArray index out of range");
         data_[index = value];
         // replace
     }
